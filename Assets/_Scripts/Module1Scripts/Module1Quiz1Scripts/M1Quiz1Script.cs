87811a34ee772411e071ad611e72f0279abe4156
ttoyryla <ttoyryla@oakland.edu>
2022-04-06 21:38:40 -0400

    Quiz functionality
    Question generation, answer generation, answer validation, etc.

--- Assets/_Scripts/Module1Scripts/Module1Quiz1Scripts/M1Quiz1Script.cs
+++ Assets/_Scripts/Module1Scripts/Module1Quiz1Scripts/M1Quiz1Script.cs
@@ -1,62 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine;
 
 public class M1Quiz1Script : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] TMP_Text scaleReading;
+    [SerializeField] TMP_Text questionDescription;
+    [SerializeField] TMP_Text answerWrongText;
+    [SerializeField] TMP_Dropdown answerDropdown;
+    [SerializeField] GameObject winScreen;
+    [SerializeField] GameObject returnWarning;
+    [SerializeField] QuizSimScript simScript;
+    [SerializeField] Canvas QuizCanvas;
+
     string question1 = "What combination of units adds up to ";
     string[] question1Prompts = new string[] { "2.143243Mg", "653.0004kg", "322.19242g", "23.1002g", "917.3131g" };
     string[] question1CorrectAnswers = { "2,143kg + 243g", "653,000g + 400mg", "322,192mg + 420ug", "23,100mg + 200ug", "917,313ug + 100ug" };
     string[] question1WrongAnswer1 = { "21,432kg + 43g", "65,300g + 40mg", "3,221,924mg + 20ug", "231,000mg + 2000ug", "91,731ug + 3100ug" };
     string[] question1WrongAnswer2 = { "214,324kg + 3g", "6,530g + 400mg", "32,219mg + 2420ug", "2,310mg + 2000ug", "9,173,100ug + 31ug" };
     string[] question1WrongAnswer3 = { "0.2143243kg", "653,400g", "322,192mg + 4,200ug", "231,020mg + 2ug", "9,173ug + 1310ug" };
+    
     int arrayIndex;
-    int prevIndex = -1;
+    //int prevIndex = -1;
     void Start()
     {
-        //arrayIndex = Random.Range(0, 4);
-
+        answerWrongText.enabled = false;
+        arrayIndex = Random.Range(0, 4);
         //Debug.Log(arrayIndex);
+        scaleReading.text = question1Prompts[arrayIndex];
+        string[] questionUnits = GetQuestionUnits();
+        float xPos = 350f;
+        for(int i = 0; i < questionUnits.Length; i++)
+        {
+            simScript.AddWeight(questionUnits[i], xPos);
+            xPos = xPos + 250f;
+        }
+        questionDescription.text = question1 + question1Prompts[arrayIndex] + "?";
+        List<string> questionAnswers = GenerateAnswerOrder();
+        answerDropdown.ClearOptions();
+        answerDropdown.AddOptions(questionAnswers);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //arrayIndex = Random.Range(0, 4);
-        //Debug.Log(arrayIndex);
+        
     }
     public void setArrayIndex()
     {
         arrayIndex = Random.Range(0, 4);
         Debug.Log(arrayIndex);
     }
-    public string GetQuestionNumber()
+    public List<string> GenerateAnswerOrder()
     {
-        string questionNum = "";
-        Debug.Log(arrayIndex + " case statement");
-        switch (arrayIndex)
+        List<string> answers;
+        int random = Random.Range(0, 3);
+        switch (random)
         {
             case 0:
-                questionNum = "2.143243Mg";
+                answers = new List<string> { question1CorrectAnswers[arrayIndex], question1WrongAnswer1[arrayIndex], question1WrongAnswer2[arrayIndex], question1WrongAnswer3[arrayIndex]};
                 break;
             case 1:
-                questionNum = "653.0004kg";
+                answers = new List<string> { question1WrongAnswer2[arrayIndex], question1CorrectAnswers[arrayIndex], question1WrongAnswer3[arrayIndex], question1WrongAnswer1[arrayIndex] };
                 break;
             case 2:
-                questionNum = "322.19242g";
+                answers = new List<string> { question1WrongAnswer2[arrayIndex], question1WrongAnswer1[arrayIndex], question1CorrectAnswers[arrayIndex], question1WrongAnswer3[arrayIndex] };
                 break;
             case 3:
-                questionNum = "23.1002g";
-                break;
-            case 4:
-                questionNum = "917.3131g";
+                answers = new List<string> { question1WrongAnswer3[arrayIndex], question1WrongAnswer2[arrayIndex], question1WrongAnswer1[arrayIndex], question1CorrectAnswers[arrayIndex]};
                 break;
             default:
-                questionNum = "???";
+                answers = new List<string> { question1CorrectAnswers[arrayIndex], question1WrongAnswer1[arrayIndex], question1WrongAnswer2[arrayIndex], question1WrongAnswer3[arrayIndex] };
                 break;
         }
-        return questionNum;
+        return answers;
+    }
+    public void HideWrongAnswerText()
+    {
+        answerWrongText.enabled = false;
+    }
+    public void CheckAnswer()
+    {
+        if(answerDropdown.options[answerDropdown.value].text == question1CorrectAnswers[arrayIndex])
+        {
+            simScript.SetSimCanvasEnabled(false);
+            winScreen.SetActive(true);
+        }
+        else
+        {
+            answerWrongText.enabled = true;
+        }
     }
     public string[] GetQuestionUnits()
     {
