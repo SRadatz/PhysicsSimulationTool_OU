735289cf5ae52b3d61e2cd49c2f1846028920436
ttoyryla <ttoyryla@oakland.edu>
2022-04-14 02:30:11 -0400

    Simulation UI and script set up

---
+++ Assets/_Scripts/Module1Scripts/Module1Exercise2Scripts/ExerciseTwoSimScript.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class ExerciseTwoSimScript : MonoBehaviour
+{
+    [SerializeField] TMP_Text AnswerTxt1;
+    [SerializeField] TMP_Text AnswerTxt2;
+    [SerializeField] TMP_Text AnswerTxt3;
+    [SerializeField] TMP_Text AnswerTxt4;
+    [SerializeField] TMP_Text AnswerTxt5;
+    [SerializeField] TMP_Text AnswerTxt6;
+    [SerializeField] TMP_Text AnswerTxt7;
+    [SerializeField] TMP_Text AnswerTxt8;
+    [SerializeField] TMP_Text AnswerTxt9;
+    [SerializeField] TMP_Text AnswerTxt10;
+    [SerializeField] TMP_Text AnswerTxt11;
+    [SerializeField] TMP_Text AnswerTxt12;
+
+    [SerializeField] GameObject DigitCheckmark1;
+    [SerializeField] GameObject DigitCheckmark2;
+    [SerializeField] GameObject DigitCheckmark3;
+    [SerializeField] GameObject DigitCheckmark4;
+    [SerializeField] GameObject DigitCheckmark5;
+    [SerializeField] GameObject DigitCheckmark6;
+    [SerializeField] GameObject DigitCheckmark7;
+    [SerializeField] GameObject DigitCheckmark8;
+    [SerializeField] GameObject DigitCheckmark9;
+    [SerializeField] GameObject DigitCheckmark10;
+    [SerializeField] GameObject DigitCheckmark11;
+    [SerializeField] GameObject DigitCheckmark12;
+
+    [SerializeField] GameObject DigitXMark1;
+    [SerializeField] GameObject DigitXMark2;
+    [SerializeField] GameObject DigitXMark3;
+    [SerializeField] GameObject DigitXMark4;
+    [SerializeField] GameObject DigitXMark5;
+    [SerializeField] GameObject DigitXMark6;
+    [SerializeField] GameObject DigitXMark7;
+    [SerializeField] GameObject DigitXMark8;
+    [SerializeField] GameObject DigitXMark9;
+    [SerializeField] GameObject DigitXMark10;
+    [SerializeField] GameObject DigitXMark11;
+    [SerializeField] GameObject DigitXMark12;
+
+    //string [] sigFigQuestions = new string[] {}
+    //string[] prompts =  new string[] {"Average velocity of a car having traveled 8.4km in 2.03s", "Force on a 12.34kg block accelerating at 5.242m/s", "Launch speed of a ball thrown at a 54 degree angle traveling 24m in 2.2s",
+    //"Revolutions per second required to give an astronaut of 29.4m/s in a circular motion w/ 9.45m radius", "Speed of a wrecking ball rotating counterclockwise with (-22/5i + 20.2j)m/s/s acceleration",
+    //"Maximum speed a light truck can go around a 75m radius curve at 32m/s", "Initial speed a 2m tall basketball player standing 10m from a 3.05m hoop must throw the ball to get nothing but net."}
+    //string[] promptAnswers =  new string[] {"0004.13793103", "0064.68628000", "0018.14602160", "0000.28075268", "0006.67008246", "0022.62741700", "0010.66395800" }
+    //string [] promptAnswerUnits = new string [] {"km/s", "N", "m/s", "rps", "m/s", "m/s", "m/s" }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
